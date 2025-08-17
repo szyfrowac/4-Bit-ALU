90b157c525d6bd74b4a8e8fff311cc0f4290e304
Bala Phanikar Challa <142141537+szyfrowac@users.noreply.github.com>
2025-08-17 12:14:56 +0530

    Delete demux.v

--- demux.v
+++
@@ -1,8 +0,0 @@
-module demux(
-    input  sel,   // opcode[0]
-    input  i,     // enable (set to 1)
-    output y0, y1 // y0 = AND enable, y1 = OR enable
-);
-    assign y0 = (sel) ? 1'b0 : i;
-    assign y1 = (sel) ? i    : 1'b0;
-endmodule
